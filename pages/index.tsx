28a9764e3f2f84f35ea75092ce0dcf449ba98cb9
cs0041 <pamzalove20@hotmail.com>
2023-01-10 22:24:47 +0700

    Landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Landing from '../components/Landing'
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +12,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-       <Header></Header>
+       <Header/>
+
+       <main className='relative h-[200vh] bg-[#E7ECEE]'>
+          <Landing/>
+       </main>
     </div>
   )
 }
