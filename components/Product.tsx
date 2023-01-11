311125da6134cd4328fc1a6a2d6cb3849a3d3b41
cs0041 <pamzalove20@hotmail.com>
2023-01-12 00:20:46 +0700

    Product add toast / dispatch

--- components/Product.tsx
+++ components/Product.tsx
@@ -2,12 +2,24 @@ import { ShoppingCartIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
 import React from 'react'
 import { urlFor } from '../sanity'
+import { useDispatch } from 'react-redux'
+import { addToBasket } from '../redux/basketSlice'
+import toast from 'react-hot-toast'
 
 interface Props {
     product: Product
 }
 
+
 function Product({ product }: Props) {
+    const dispatch = useDispatch()
+    const addItemToBasket = () => {
+        dispatch(addToBasket(product))
+
+        toast.success(`${product.title} added to basket`, {
+          position: 'bottom-center',
+        })
+    }
   return (
     <div
       className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl 
@@ -28,9 +40,12 @@ function Product({ product }: Props) {
           <p>{product.price}</p>
         </div>
 
-        <div className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center 
+        <div
+          className="flex h-16 w-16 flex-shrink-0 cursor-pointer items-center 
         justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 
-        md:h-[70px] md:w-[70px]">
+        md:h-[70px] md:w-[70px]"
+          onClick={addItemToBasket}
+        >
           <ShoppingCartIcon className="h-8 w-8 text-white" />
         </div>
       </div>
